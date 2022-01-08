f8649eae4774fecad4caf8f879720d0374956639
Elvira A.Kulikova <e.kulikova@norsi-trans.ru>
2022-01-08 09:59:46 +0300

    simple

--- test_functions.py
+++ test_functions.py
@@ -5,5 +5,17 @@ class TestFunctions(TestCase):
     def test_divide_result(self):
         divident = 15
         divisor = 3
-        expected_result = 4.0
-        self.assertAlmostEqual(divide(divident, divisor), expected_result)
+        expected_result = 5.0
+        self.assertAlmostEqual(divide(divident, divisor), expected_result, delta=0.0001)
+
+    def test_divide_negative(self):
+        divident = 15
+        divisor = -3
+        expected_result = -5.0
+        self.assertAlmostEqual(divide(divident,divisor), expected_result, delta=0.0001)
+
+    def test_divide_zero(self):
+        divident = 0
+        divisor = 5
+        expected_result = 0
+        self.assertAlmostEqual(divide(divident,divisor), expected_result)
