69abd73112b558be10128e0580e30305c02b956d
evulet <elvirochkata@gmail.com>
2022-01-10 11:48:08 +0300

    testing with multiplication function

--- functions.py
+++ functions.py
@@ -5,3 +5,13 @@ def divide(divident: Union[int, float], divisor: Union[int, float]):
     if divisor == 0:
         raise ValueError("The divisor cannot be zero.")
     return divident / divisor
+
+def multiply(*args: Union[int, float]):
+    if len(args) == 0:
+        raise ValueError("At least one value to multiply must be passed.")
+
+    total = 1
+    for arg in args:
+        total *= arg
+
+    return total
